a18887101623bb487880102e60828865ecb759ed
Tomasz Nyżnyk <tomasznyznyk@gmail.com>
2019-02-22 14:20:34 +0100

    add fonts. add character selection. add character image, name and description display.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,90 @@
 $(function() {
 
+    var mainApiUrl = "https://gateway.marvel.com:443/v1/public/characters?limit=100&apikey=c767374de595fc64ed5dea718a47f2d1";
+    var characterApiUrl = "";
     var characterList = $(".characterList");
-    var body = $(".body");
-    var apiUrl = "https://gateway.marvel.com:443/v1/public/characters?limit=5&apikey=c767374de595fc64ed5dea718a47f2d1";
 
     function insertListContent(characters) {
 
         for (var i = 0; i < characters.length; i++) {
             characterList.append(`
-                <li>
+                <option id = ${i}>
                     ${characters[i].name}
-                </li>
-            `);
-
-            body.append(`
-                <img class="thumbnail" src="${characters[i].thumbnail.path}.${characters[i].thumbnail.extension}">
+                </option>
             `);
         }
-
     }
 
-    function loadApi() {
-        $.ajax(apiUrl)
+    function insertCharacterContent(character) {
+
+        var thumbnailHolder = $(".thumbnailHolder");
+        var nameHolder = $(".nameHolder");
+        var infoHolder = $(".infoHolder");
+
+        thumbnailHolder.replaceWith(`
+            <div class="thumbnailHolder">
+                <img class="thumbnail" src="${character.thumbnail.path}.${character.thumbnail.extension}">
+            </div>
+        `);
+
+        nameHolder.replaceWith(`
+            <div class="nameHolder">
+                <p class="name">${character.name}</p>
+            </div>    
+        `);
+
+        console.log(character.description);
+
+        if(character.description === "") {
+            infoHolder.replaceWith(`
+                <div class="infoHolder">
+                    <p class="infoTitle">Description:</p>
+                    <p class="info">DESCRIPTION NOT FOUND</p>
+                </div>
+            `);
+        } else {
+            infoHolder.replaceWith(`
+                <div class="infoHolder">
+                    <p class="infoTitle">Description:</p>
+                    <p class="info">${character.description}</p>
+                </div>
+            `);
+        }
+    }
+
+    function charactersList() {
+        $.ajax(mainApiUrl)
             .done(function(data) {
                 console.log(data);
 
                 var characters = data.data.results;
 
                 insertListContent(characters);
+
+                characterList.on("change", function() {
+
+                    var selectedOptionId = $(this).children(":selected").attr("id");
+
+                    characterApiUrl = characters[selectedOptionId].resourceURI + "?apikey=c767374de595fc64ed5dea718a47f2d1";
+
+                    console.log("I work");
+
+                    loadCharacter();
+                });
             });
     }
 
-    loadApi();
+    function loadCharacter() {
+
+        $.ajax(characterApiUrl)
+            .done(function(data) {
+                console.log(data);
+
+                var character = data.data.results[0];
+
+                insertCharacterContent(character);
+            });
+    }
+
+    charactersList();
 });
