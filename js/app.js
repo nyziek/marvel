0d7d1ed971b91bfa15390fe65a763ebb4982205a
Tomasz Nyżnyk <tomasznyznyk@gmail.com>
2019-02-26 02:37:58 +0100

    optimize code. switch to ES6. minor styling changes to loading splash page. add footer with data source information. change comic info Api request.

--- js/app.js
+++ js/app.js
@@ -1,42 +1,81 @@
 $(function() {
 
-    $(document).ready(function () {
-        $(document).ajaxStart(function () {
-            $("#loading").show();
-            function runAnimation() {
-                const spiderman = $("#spiderman");
-                spiderman.animate({opacity:'0.3'}, 1000);
-                spiderman.animate({opacity:'1'}, 1000, runAnimation);
-            }
-            runAnimation();
-        }).ajaxStop(function () {
-            $("#loading").hide();
-            $("#spiderman").stop();
-        });
-    });
+// VARIABLES
+
+    // List of first letters of characters' names
 
     const letters = ["3", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     let firstLettersList = $(".firstLettersList");
     let chosenLetter = "3";
 
+    // APIs
+
     let mainApiUrl = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=3&limit=100&apikey=c767374de595fc64ed5dea718a47f2d1";
+    let characterApiUrl = "";
+
+    // Character list
 
     let characters = [];
+    let characterList = $(".characterList");
 
-    for(var i = 0; i < letters.length; i++) {
+    // Chosen character info
 
-        if(letters[i] === chosenLetter) {
-            firstLettersList.append(`
-            <li class="chosenLetter">${letters[i]}</li>
+    const thumbnailHolder = $(".thumbnailHolder");
+    const nameHolder = $(".nameHolder");
+    const infoHolder = $(".infoHolder");
+
+    // Comics list
+
+    const comicsListHolder = $(".comicsListHolder");
+
+    // Data loading page
+
+    const loadingPage = $("#loading");
+    const spiderman = $("#spiderman");
+    const loadingText = $("#loadingText");
+
+// METHODS
+
+    // Data loading page
+
+    const loadingPageAnimation = () => {
+        spiderman.animate({opacity:'0.4'}, 700);
+        loadingText.animate({opacity:'0.3'}, 700);
+        spiderman.animate({opacity:'0.9'}, 700);
+        loadingText.animate({opacity:'0.8'}, 700, loadingPageAnimation);
+    };
+
+    const showLoadingPage = () => {
+        loadingPage.show();
+        loadingPageAnimation();
+    };
+
+    const hideLoadingPage = () => {
+        loadingPage.hide();
+        spiderman.stop();
+        loadingText.stop();
+    };
+
+    // List of first letters of characters' names - list display
+
+    const displayLettersList = () => {
+
+        letters.forEach(letter => {
+            if(letter === chosenLetter) {
+                firstLettersList.append(`
+            <li class="chosenLetter">${letter}</li>
             `);
-        } else {
-            firstLettersList.append(`
-            <li>${letters[i]}</li>
+            } else {
+                firstLettersList.append(`
+            <li>${letter}</li>
             `);
-        }
-    }
+            }
+        });
+    };
 
-    $(".firstLettersList li").on("click", function(event) {
+    // List of first letters of characters' names - highlighted letter change
+
+    function changeLetter() {
 
         const prevChosenLetter = $(".chosenLetter")[0];
 
@@ -51,27 +90,43 @@ $(function() {
         characterList.html(``);
 
         charactersList();
-    });
-
-    var characterApiUrl = "";
-    var characterList = $(".characterList");
-
-    function insertListContent(characters) {
-
-        characterList.append(`
-                <option></option>
-            `);
-
-        for (var i = 0; i < characters.length; i++) {
-            characterList.append(`
-                <option id = ${i}>${characters[i].name}</option>
-            `);
-        }
     }
 
-    function insertComicContent(comic) {
+    // Characters' names list
 
-        var comicsList = $(".comicsList");
+    const insertListContent = characters => {
+
+        characterList.append(`
+            <option></option>
+        `);
+
+        let id = 0;
+
+        characters.forEach(character => {
+
+            characterList.append(`
+                <option id = ${id}>${character.name}</option>
+            `);
+
+            id++;
+        });
+    };
+
+    const charactersList = () => {
+        $.ajax(mainApiUrl)
+            .done(function(data) {
+
+                characters = data.data.results;
+
+                insertListContent(characters);
+            });
+    };
+
+    // Chosen character info
+
+    const insertComicContent = comic => {
+
+        const comicsList = $(".comicsList");
 
         comicsList.append(`
                 <li class="comicsListItem">
@@ -79,13 +134,9 @@ $(function() {
                     <p class="comicInfo">${comic[0].title}</p>
                 </li>
             `);
-    }
+    };
 
-    function insertCharacterContent(character) {
-
-        var thumbnailHolder = $(".thumbnailHolder");
-        var nameHolder = $(".nameHolder");
-        var infoHolder = $(".infoHolder");
+    const insertCharacterContent = character => {
 
         thumbnailHolder.html(`
             <img class="thumbnail" src="${character.thumbnail.path}.${character.thumbnail.extension}">
@@ -107,69 +158,89 @@ $(function() {
             `);
         }
 
-        var comicsListHolder = $(".comicsListHolder");
-
         comicsListHolder.html(`
             <p class="title">Apears in:</p>
             <ul class="comicsList">
             </ul>
         `);
 
-        for (var i = 0; i < character.comics.items.length; i++) {
+        const collectionApiUrl = character.comics.collectionURI + "?limit=20&apikey=c767374de595fc64ed5dea718a47f2d1";
 
-            var comicApiUrl = character.comics.items[i].resourceURI + "?apikey=c767374de595fc64ed5dea718a47f2d1";
+        let collection = [];
 
-            comicApiUrl = comicApiUrl.replace("http", "https");
-
-            console.log(comicApiUrl);
-
-            $.ajax(comicApiUrl)
-                .done(function(data) {
-
-                    var comic = data.data.results;
-                    
-                    console.log(comic);
-
-                    insertComicContent(comic);
-                });
-        }
-    }
-
-    function charactersList() {
-        $.ajax(mainApiUrl)
+        $.ajax(collectionApiUrl)
             .done(function(data) {
 
-                characters = data.data.results;
+                collection = data.data.results;
 
-                insertListContent(characters);
+                console.log(collection);
 
-                characterList.on("change", function() {
+                collection.forEach(comic => {
+                    let comicApiUrl = comic.resourceURI + "?apikey=c767374de595fc64ed5dea718a47f2d1";
 
-                    var selectedOptionId = $(this).children(":selected").attr("id");
-                    
-                    characterApiUrl = characters[selectedOptionId].resourceURI + "?apikey=c767374de595fc64ed5dea718a47f2d1";
+                    comicApiUrl = comicApiUrl.replace("http", "https");
 
-                    characterApiUrl = characterApiUrl.replace("http", "https");
+                    $.ajax(comicApiUrl)
+                        .done(function(data) {
 
-                    console.log(characterApiUrl);
+                            const comicInfo = data.data.results;
 
-                    loadCharacter();
+                            insertComicContent(comicInfo);
+                        });
                 });
             });
-    }
+    };
 
-    function loadCharacter() {
+    const loadCharacter = () => {
 
         $.ajax(characterApiUrl)
             .done(function(data) {
 
-                var character = data.data.results[0];
+                const character = data.data.results[0];
 
                 console.log(character);
 
                 insertCharacterContent(character);
             });
-    }
+    };
+
+    const changeCharacter = () => {
+
+        const selectedOptionId = characterList.children(":selected").attr("id");
+
+        if(characters[selectedOptionId] !== undefined) {
+
+            characterApiUrl = characters[selectedOptionId].resourceURI + "?apikey=c767374de595fc64ed5dea718a47f2d1";
+            characterApiUrl = characterApiUrl.replace("http", "https");
+
+            loadCharacter();
+        }
+    };
+
+// PAGE LOGIC
+
+    // Loading page display
+
+    $(document).ajaxStart(
+        showLoadingPage
+    ).ajaxStop(
+        hideLoadingPage
+    );
+
+    // Displaying first letters of name list
+
+    displayLettersList();
+
+    // Changing chosen first letter of name
+
+    firstLettersList.on("click", "li", changeLetter);
+
+    // Inserting characters list options on page load
 
     charactersList();
+
+    // Choosing character from characters list
+
+    characterList.on("change", changeCharacter);
+
 });
